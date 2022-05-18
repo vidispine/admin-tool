1d296695bf70d4225b58576f59023fb9ef25b30b
Alex Jennings <alexjennings@users.noreply.github.com>
2022-05-17 17:17:37 -0700

    Style console message

--- src/utils/setWelcomeConsoleMessage.js
+++ src/utils/setWelcomeConsoleMessage.js
@@ -1,8 +1,17 @@
 /* eslint-disable no-console */
 import * as VidiCore from '@vidispine/vdt-api';
 
+const styles = `
+    color:white;
+    background-color:black;
+    padding: 8px;
+    font-weight: 600;
+    font-family: Courier;
+    font-size: 16px;
+`;
+
 const setWelcomeConsoleMessage = () => {
-  console.info('Welcome to the VidiCore admin-tool!  Try using the "VidiCore" global object');
+  console.info('%c🎉🎉🎉 Welcome to the VidiCore admin-tool!  🎉🎉🎉\n👻👻👻    Try using the "VidiCore" object   👻👻👻', styles);
   window.VidiCore = Object.keys(VidiCore).reduce((acc, cur) => {
     const val = Object.keys(VidiCore[cur]).reduce((thisAcc, thisCur) => {
       const thisVal = VidiCore[cur][thisCur];
