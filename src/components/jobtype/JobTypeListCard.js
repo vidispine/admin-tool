e58dda655039589d99101177f03cfaccc1f13c37
Alex Jennings <alexjennings@users.noreply.github.com>
2023-01-17 10:47:10 -0800

    Adds option for sorting uri lists

--- src/components/jobtype/JobTypeListCard.js
+++ src/components/jobtype/JobTypeListCard.js
@@ -4,6 +4,8 @@ import CardContent from '@material-ui/core/CardContent';
 import SquareCard from '../ui/SquareCard';
 import UriListTable from '../ui/UriListTable';
 
+import sortCaseInsensitive from '../../utils/sortCaseInsensitive';
+
 export default function JobTypeListCard({
   uriListDocument,
 }) {
@@ -14,6 +16,7 @@ export default function JobTypeListCard({
         <UriListTable
           uriListDocument={uriListDocument}
           linkTo={linkTo}
+          sort={sortCaseInsensitive}
         />
       </CardContent>
     </SquareCard>
