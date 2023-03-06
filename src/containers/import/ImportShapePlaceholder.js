1fa7a0edc89fd302ecce6d4fbb91b3b6a0b55f84
Alex Jennings <alexjennings@users.noreply.github.com>
2023-03-06 08:50:35 -0500

    Redirect to new placeholder shape

--- src/containers/import/ImportShapePlaceholder.js
+++ src/containers/import/ImportShapePlaceholder.js
@@ -13,12 +13,18 @@ class ImportShapePlaceholder extends React.PureComponent {
   }
 
   render() {
-    const { location } = this.props;
+    const { location, history } = this.props;
     const query = new URLSearchParams(location.search);
     const itemId = query.get('itemId');
+    const onSuccess = (response, dispatch, props) => {
+      const shapeId = response?.data;
+      const valueItemId = props?.values?.itemId;
+      history.push(`/item/${valueItemId}/shape/${shapeId}/`);
+    };
     return (
       <ImportShapePlaceholderWizard
         initialValues={{ itemId }}
+        onSuccess={onSuccess}
         {...this.props}
       />
     );
