bfb6062b93ff673e516f1428921d9f62409b34fc
Alex Jennings <alexjennings@users.noreply.github.com>
2023-06-02 07:57:16 -0700

    Set correct param for keepShapeTagStorage when deleting item

--- src/components/item/ItemDeleteForm.js
+++ src/components/item/ItemDeleteForm.js
@@ -5,18 +5,30 @@ import { TextField } from '../form';
 
 import FormSection from '../ui/FormSection';
 import Field from '../ui/Field';
+import FieldTypeArray from '../ui/FieldTypeArray';
+import { KeyValuePairType } from '../ui/FormType';
 
 const queryParams = () => (
   <>
     <Field
       name="keepShapeTagMedia"
+      label="Keep Shape-Tag Media"
       component={TextField}
       fullWidth
     />
     <Field
-      name="Storage"
+      name="keepShapeTagStorage"
+      label="Keep Shape-Tag Storage"
       component={TextField}
       fullWidth
+    />
+    <FieldTypeArray
+      name="notificationData"
+      component={KeyValuePairType}
+      label="Notification Metadata"
+      arrayHeader
+      withHeader={false}
+      dense
     />
   </>
 );
