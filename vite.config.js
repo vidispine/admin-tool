f649634175068c26fa588c3fb081aa1854bb24f4
Alex Jennings <alexjennings@users.noreply.github.com>
2023-08-10 12:56:18 -0700

    Sets vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,42 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const VIDISPINE_ENDPOINTS = ['/API/', '/APInoauth/', '/APIinit/', '/APIdoc/', '/UploadLicense/'];
+const VIDISPINE_URL = process.env.VITE_VIDISPINE_URL === '' ? undefined : process.env.VITE_VIDISPINE_URL;
+const proxyOptions = {
+  target: VIDISPINE_URL,
+  changeOrigin: true,
+  selfHandleResponse: false,
+  configure: (proxy) => {
+    proxy.on('proxyRes', (proxyRes) => {
+      // eslint-disable-next-line no-param-reassign
+      delete proxyRes.headers['www-authenticate'];
+    });
+  },
+};
+
+const proxy = VIDISPINE_URL
+  ? VIDISPINE_ENDPOINTS.reduce((a, c) => ({ ...a, [c]: proxyOptions }), {})
+  : undefined;
+
+const base = process.env.PUBLIC_URL === '' ? '/' : process.env.PUBLIC_URL;
+
 export default ({ mode }) => defineConfig({
-  plugins: [react()],
+  base,
+  server: {
+    port: 3000,
+    open: true,
+    proxy,
+  },
+  plugins: [splitVendorChunkPlugin(), react()],
   define: {
     'process.env.NODE_ENV': `"${mode}"`,
+  },
+  build: {
+    outDir: 'build',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
 });
