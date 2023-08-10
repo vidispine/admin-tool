eae67430b4a35e60a2656d4899f99b891a56c5e0
Alex Jennings <alexjennings@users.noreply.github.com>
2023-08-10 12:58:47 -0700

    Rename js to jsx

---
+++ src/components/ui/VidispineIcon.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+// import { ReactComponent as headerLogo } from '../../assets/header-logo.svg';
+
+function VidispineIcon(props) {
+  return (
+    // <SvgIcon {...props} component={headerLogo} viewBox="0 0 76.64 76.21">
+    <SvgIcon {...props} viewBox="0 0 76.64 76.21">
+      <defs>
+        <style>
+          { '.cls-1{fill:#7c74bd;}.cls-2{fill:#6dc3c5;}.cls-3{fill:#0068a9;}.cls-4{fill:#44a8aa;fill-rule:evenodd;}'}
+        </style>
+      </defs>
+      <title>Vidispine-icon-RGB</title>
+      <g id="Layer_2" data-name="Layer 2">
+        <g id="Layer_1-2" data-name="Layer 1-2">
+          <rect className="cls-1" width="21.3" height="21.35" />
+          <rect className="cls-2" x="55.27" y="0.01" width="21.36" height="21.35" />
+          <rect className="cls-3" y="27.19" width="21.3" height="21.35" />
+          <path className="cls-4" d="M55.27,48.54A21.36,21.36,0,0,0,76.64,27.19H55.27Z" />
+          <rect className="cls-3" y="54.86" width="21.3" height="21.35" />
+          <path className="cls-4" d="M27.61,76.21A21.35,21.35,0,0,0,49,54.86H27.61Z" />
+        </g>
+      </g>
+
+    </SvgIcon>
+  );
+}
+
+export default VidispineIcon;
