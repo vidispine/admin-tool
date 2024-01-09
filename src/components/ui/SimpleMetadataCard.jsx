d8cc8042a1f7a6f6e3a8214589136b09c5e78c72
Alex Jennings <alexjennings@users.noreply.github.com>
2024-01-09 14:28:45 -0800

    Returns padding to simplemetadata

--- src/components/ui/SimpleMetadataCard.jsx
+++ src/components/ui/SimpleMetadataCard.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 
 import SquareCard from './SquareCard';
 import SimpleMetadataEditor from './SimpleMetadataEditor';
 
-export default function SimpleMetadataCard(props) {
+const styles = (theme) => ({
+  root: {
+    padding: theme.spacing(2),
+    '&:last-child': {
+      paddingBottom: theme.spacing(3),
+    },
+  },
+});
+
+function SimpleMetadataCard({ classes, ...props }) {
   return (
-    <>
-      <SquareCard>
-        <SimpleMetadataEditor
-          {...props}
-        />
-      </SquareCard>
-    </>
+    <SquareCard classes={classes}>
+      <SimpleMetadataEditor
+        {...props}
+      />
+    </SquareCard>
   );
 }
+
+export default withStyles(styles)(SimpleMetadataCard);
