34148c6de16c101f15098d6c8ac01fbd1f8b053d
Alex Jennings <alexjennings@users.noreply.github.com>
2025-08-26 15:00:29 -0700

    Adds filter when there is no native search

--- src/components/metadatadataset/MetadataDatasetListCard.jsx
+++ src/components/metadatadataset/MetadataDatasetListCard.jsx
@@ -1,15 +1,33 @@
+import { useState } from 'react';
+
+import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
-import SquareCard from '../ui/SquareCard';
+import FilterTextField from '../ui/FilterTextField';
 
 import MetadataDatasetListTable from './MetadataDatasetListTable';
 
-export default function MetadataDatasetListCard({ ...props }) {
+export default function MetadataDatasetListCard({ metadataDatasetListDocument, ...props }) {
+  const options = metadataDatasetListDocument?.dataset || [];
+  const [filter, setFilter] = useState(options);
+  const filterMetadataDatasetListDocument = { ...metadataDatasetListDocument, dataset: filter };
   return (
-    <SquareCard>
+    <Card>
       <CardContent>
-        <MetadataDatasetListTable {...props} />
+        <FilterTextField
+          variant="outlined"
+          fullWidth
+          options={options}
+          onChange={setFilter}
+          optionsKey="name"
+          minScore={0.5}
+        />
+
+        <MetadataDatasetListTable
+          metadataDatasetListDocument={filterMetadataDatasetListDocument}
+          {...props}
+        />
       </CardContent>
-    </SquareCard>
+    </Card>
   );
 }
